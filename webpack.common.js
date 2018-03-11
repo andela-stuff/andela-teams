bd7f4f9288b15deef831fa6ec18577ec6e1110b7
Franklin Chieze <chieze.franklin@gmail.com>
2018-03-11 07:54:47 +0100

    add 'url-loader' and 'file-loader'

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,35 @@
+const Dotenv = require('dotenv-webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const fs = require('fs');
 const path = require('path');
+const webpack = require('webpack');
 
-const nodeModules = {};
+/*const nodeModules = {};
 fs.readdirSync('node_modules')
   .filter(x => ['.bin'].indexOf(x) === -1)
   .forEach((mod) => {
     nodeModules[mod] = `commonjs ${mod}`;
-  });
+  });*/
 
 module.exports = {
   entry: './src/app.js',
-  devtool: 'sourcemap',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'app.js'
+  },
+  plugins: [
+    new Dotenv({ systemvars: true }),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css', '.scss'],
+    descriptionFiles: ['package.json']
   },
   module: {
     rules: [
@@ -26,9 +42,20 @@ module.exports = {
         }
       },
       {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        loader: 'style-loader!css-loader!sass-loader'
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(tff|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader?limit=250000'
       }
     ]
   },
