9645e91034765f379f9c5b545972d1e28bc55993
Franklin Chieze <chieze.franklin@gmail.com>
2018-03-21 22:47:08 +0100

    add style to make footer stay at the bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,18 @@
 
 import React from 'react';
 
+const style = {
+  position: 'absolute',
+  right: 0,
+  bottom: 0,
+  left: 0,
+  padding: '1rem',
+  backgroundColor: '#efefef',
+  textAlign: 'center'
+};
+
 const Footer = () => (
-  <footer className="al-footer clearfix">
+  <footer className="al-footer clearfix" style={style}>
     <div className="al-footer-right">Created with <i className="ion-heart" /></div>
     <div className="al-footer-main clearfix">
       <div className="al-copy">React Webpack Skeleton</div>
