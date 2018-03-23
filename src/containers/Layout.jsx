39d29ae4b525fde1305edab5bb8806c209f9d7c1
Franklin Chieze <chieze.franklin@gmail.com>
2018-03-23 21:23:02 +0100

    resolve issue with Layout (take 1)

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -3,16 +3,21 @@
  *
  * @author Franklin Chieze
  *
+ * @requires NPM:lodash
+ * @requires NPM:prop-types
  * @requires NPM:react
  * @requires ../components/PageTop.jsx
  * @requires ../components/Sidebar.jsx
  */
 
+import _ from 'lodash';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import Footer from '../components/Footer.jsx';
 import PageTop from '../components/PageTop.jsx';
 import Sidebar from '../components/Sidebar.jsx';
+// import eventBus from '../lib/EventBus';
 
 /**
 * Layout component
@@ -34,6 +39,24 @@ class Layout extends React.Component {
   }
 
   /**
+   * @method create
+   * @desc This method creates a new team
+   *
+   * @param { object} req request
+   * @param { object} res response
+   *
+   * @returns { object } response
+   */
+  componentWillMount() {
+    /* if (process.env.AUTH0_PUB_KEY) {
+      this.lock = new Auth0Lock(process.env.AUTH0_PUB_KEY, process.env.AUTH0_DOMAIN);
+      this.setState({idToken: this.getIdToken()}); // Must come after this.lock init
+    } */
+
+    // eventBus.on('logout', () => this.onLogout());
+  }
+
+  /**
    * @method render
    * @desc render method
    *
@@ -46,6 +69,17 @@ class Layout extends React.Component {
           <Sidebar {...this.props} />
           <PageTop location={this.props.location} user={this.state.user} />
 
+          <div className="al-main">
+            <div className="al-content">
+              {
+                /* React.cloneElement(
+                  this.props.children,
+                  _.assign({}, this.props, { user: this.state.user })
+                ) */
+              }
+            </div>
+          </div>
+
           <Footer />
           <back-top />
         </main>
@@ -53,5 +87,21 @@ class Layout extends React.Component {
     );
   }
 }
+
+Layout.defaultProps = {
+  location: { pathname: '/', query: {} },
+  router: { pathname: '/', query: {} }
+};
+
+Layout.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+    query: PropTypes.object.isRequired,
+  }),
+  router: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+    query: PropTypes.object.isRequired,
+  })
+};
 
 export default Layout;
